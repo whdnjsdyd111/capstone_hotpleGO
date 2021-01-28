75a496767521c73ccf6465073a9e5527f8592ab5
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-01-28 15:36:26 +0900

    home 테스트

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.AllianceRepository;
+import com.example.demo.service.implement.AllianceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class MainController {
+    @Autowired
+    AllianceImpl alliance;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("all", alliance.getList().get(0));
         return "home";
     }
 }
--- src/main/java/com/example/demo/service/implement/AllianceImpl.java
+++ src/main/java/com/example/demo/service/implement/AllianceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.implement;
 
 import com.example.demo.domain.AllianceVO;
-import com.example.demo.mapper.AllianceMapper;
+import com.example.demo.repository.AllianceRepository;
 import com.example.demo.service.AllianceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +11,11 @@ import java.util.List;
 @Service
 public class AllianceImpl implements AllianceService {
     @Autowired
-    private AllianceMapper mapper;
+    private AllianceRepository repository;
 
     @Override
     public List<AllianceVO> getList() {
-        List<AllianceVO> list = mapper.findAll();
+        List<AllianceVO> list = repository.findAll();
         return list;
     }
 }
