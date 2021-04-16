eb9824a11418043e55a51d5fd275a916ce4ea3cf
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-04-16 09:13:49 +0900

    안드로이드

--- src/main/java/com/example/demo/controller/android/AndroidCommonController.java
+++ src/main/java/com/example/demo/controller/android/AndroidCommonController.java
@@ -1,22 +1,61 @@
 package com.example.demo.controller.android;
 
+import com.example.demo.domain.ManagerVO;
 import com.example.demo.domain.UserVO;
+import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
 @RequestMapping("/android/*")
+@RequiredArgsConstructor
 @Log4j2
 public class AndroidCommonController {
+    private final UserService users;
 
     @PostMapping("/login")
-    @ResponseBody
-    public String login(@RequestBody UserVO vo) {
+    public String login(HttpServletRequest request) {
+        String id = request.getParameter("id");
+        String pw = request.getParameter("pw");
+
+        UserVO vo = users.getByEmail("id");
+        if (new BCryptPasswordEncoder().matches(pw, vo.getPw())) {
+            JSONObject obj = new JSONObject();
+            obj.put("user", vo);
+            obj.put("message", true);
+            return obj.toJSONString();
+        } else {
+            return "{message: false}";
+        }
+    }
+
+    @PostMapping("/userJoin")
+    public String userJoin(UserVO vo) {
         log.info(vo);
-        return null;
+
+        vo.setUCode(vo.getUCode() + "/U/");
+        if (users.register(vo)) {
+            return "{message: true}";
+        } else {
+            return "{message: false}";
+        }
+    }
+
+    @PostMapping("/managerJoin")
+    public String managerJoin(ManagerVO vo) {
+        log.info(vo);
+
+        vo.setUCode(vo.getUCode() + "/M/");
+        if (users.registerManager(vo)) {
+            return "{message: true}";
+        } else {
+            return "{message: false}";
+        }
     }
 }
