c194151d99aca417ee7d2f169022523b2db314c0
theGreatKang98 <srouc159@gamil.com>
2021-05-06 13:53:51 +0900

    admin 작업

--- src/main/resources/static/css/admin/adm_member.js
+++ src/main/resources/static/css/admin/adm_member.js
@@ -1,22 +1,26 @@
-$(function (){
-    $(document).on('click','.member-action-btn',function (){
-        $('#content_modal').load('/admin/content_modal?uCode=' + $(this).next().val());
-        const arr = $(this).parent().prevAll();
+$(function () {
+    $(document).on('click', '.member-action-btn',async function () {
+        let ele = this;
+            $('#content_modal').load('/admin/content_modal?uCode=' + $(ele).next().val(),function (){
+                const arr = $(ele).parent().prevAll();
+                $('#mem-role').val($(arr[0]).text());
+                $('#mem-regDate').val($(arr[1]).text());
+                $('#mem-email').val($(arr[2]).text());
+                $('#mem-nick').val($(arr[3]).text());
 
-        $('#content_modal').load(function() {
-            $('#mem-role').val($(arr[0]).text());
-            $('#mem-regDate').val($(arr[1]).text());
-            $('#mem-email').val($(arr[2]).text());
-            $('#mem-nick').val($(arr[3]).text());
-            $('#user-action').modal('show');
-            console.log($('#user-action'));
-        });
+                $('#user-action').modal('toggle');
+            });
     });
-    $(document).on('click','.warn-authority-btn',function (){
-       const arr = $(this).parent().prevAll();
-       $('#input-role').val($(arr[1]).text());
-       $('#mem-nick-sec').val($(arr[4]).text());
+    $(document).on('click', '.warn-authority-btn', function () {
+        const arr = $(this).parent().prevAll();
+        $('#input-role').val($(arr[1]).text());
+        $('#mem-nick-sec').val($(arr[4]).text());
         $('#mem-email-sec').val($(arr[3]).text());
         $('#mem-regDate-sec').val($(arr[2]).text());
     });
+    /*$(document).on('click', '#modal-close', function () {
+        $('#user-action').modal('hide');
+        $('#content_modal').html('');
+    });*/
 });
+
